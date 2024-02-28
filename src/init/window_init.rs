5240154bcfefcef6bb70f4f059fd177f952ba8ed
LupenGithub <lupenleft@gmail.com>
2024-02-28 22:42:52 +0100

    Even more cool stuff

--- src/init/window_init.rs
+++ src/init/window_init.rs
@@ -23,6 +23,8 @@ async fn run(mut window_config: WindowConfig) -> WindowConfig{
                 if new_size.width > 0 && new_size.height > 0{
                     window_config.size.width = new_size.width;
                     window_config.size.height = new_size.height;
+                    window_config.config.width = new_size.width;
+                    window_config.config.height = new_size.height;
                     window_config.surface.configure(&window_config.device, &window_config.config);
                     println!("{},{}", window_config.size.width, window_config.size.height);
                 }
@@ -33,6 +35,40 @@ async fn run(mut window_config: WindowConfig) -> WindowConfig{
                     window_config.surface.configure(&window_config.device, &window_config.config);
                     println!("{},{}", window_config.size.width, window_config.size.height);
                 }
+            }, Event::RedrawRequested(window_id) =>{
+                // Update here
+
+                //Render
+                let output_texture = window_config.surface.get_current_texture().unwrap();
+                let view = output_texture.texture.create_view(&wgpu::TextureViewDescriptor::default());
+                let mut encoder = window_config.device.create_command_encoder(&wgpu::CommandEncoderDescriptor{
+                    label: Some("Render Encoder")
+                }); 
+                {
+                    let render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor { 
+                        label: Some("Render Pass"), 
+                        color_attachments: &[Some(wgpu::RenderPassColorAttachment{
+                            view: &view,
+                            resolve_target: None,
+                            ops: wgpu::Operations{
+                                load: wgpu::LoadOp::Clear(wgpu::Color{
+                                    r: 0.1,
+                                    g: 0.3,
+                                    b: 0.4,
+                                    a: 1.0,
+                                }),
+                                store: wgpu::StoreOp::Store,
+                            }
+                        })], 
+                        depth_stencil_attachment: None, 
+                        timestamp_writes: None, 
+                        occlusion_query_set: None });
+                }
+
+                window_config.queue.submit(std::iter::once(encoder.finish()));
+                output_texture.present();
+            }, Event::MainEventsCleared => {
+                window_config.window.request_redraw();
             },
             _ => ()
         }
