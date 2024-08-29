6f82da4053a15039344e00cde98f01e0e84c60a5
Jachdich <jamesedkitching@gmail.com>
2024-08-30 00:10:17 +0100

    heightmap + (stolen) better camera + shitty basic fbm perlin noise generation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use cgmath::{Matrix4, Point3, Vector3};
+use std::{f32::consts::FRAC_PI_2, time::Duration};
+
+use cgmath::{perspective, InnerSpace, Matrix4, Point3, Rad, SquareMatrix, Vector3};
+use noise::{NoiseFn, Perlin};
 use wgpu::util::DeviceExt;
 use winit::{
-    dpi::PhysicalSize,
-    event::{Event, WindowEvent},
+    dpi::{PhysicalPosition, PhysicalSize},
+    event::{
+        DeviceEvent, ElementState, Event, KeyEvent, MouseButton, MouseScrollDelta, WindowEvent,
+    },
     event_loop::EventLoop,
+    keyboard::{KeyCode, PhysicalKey},
     window::{Window, WindowBuilder},
 };
 mod texture;
@@ -19,35 +25,198 @@ pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
     0.0, 0.0, 0.0, 1.0,
 );
 
-struct Camera {
-    eye: Point3<f32>,
-    target: Point3<f32>,
-    up: Vector3<f32>,
+pub struct Camera {
+    pub position: Point3<f32>,
+    yaw: Rad<f32>,
+    pitch: Rad<f32>,
+}
+
+struct Projection {
     aspect: f32,
-    fovy: f32,
+    fovy: Rad<f32>,
     znear: f32,
     zfar: f32,
 }
 
 impl Camera {
-    fn build_view_projection_matrix(&self) -> Matrix4<f32> {
-        let view = Matrix4::look_at_rh(self.eye, self.target, self.up);
-        let projection =
-            cgmath::perspective(cgmath::Rad(self.fovy), self.aspect, self.znear, self.zfar);
-        OPENGL_TO_WGPU_MATRIX * projection * view
+    pub fn new<V: Into<Point3<f32>>, Y: Into<Rad<f32>>, P: Into<Rad<f32>>>(
+        position: V,
+        yaw: Y,
+        pitch: P,
+    ) -> Self {
+        Self {
+            position: position.into(),
+            yaw: yaw.into(),
+            pitch: pitch.into(),
+        }
+    }
+
+    pub fn calc_matrix(&self) -> Matrix4<f32> {
+        let (sin_pitch, cos_pitch) = self.pitch.0.sin_cos();
+        let (sin_yaw, cos_yaw) = self.yaw.0.sin_cos();
+
+        Matrix4::look_to_rh(
+            self.position,
+            Vector3::new(cos_pitch * cos_yaw, sin_pitch, cos_pitch * sin_yaw).normalize(),
+            Vector3::unit_y(),
+        )
+    }
+}
+
+impl Projection {
+    pub fn new<F: Into<Rad<f32>>>(width: u32, height: u32, fovy: F, znear: f32, zfar: f32) -> Self {
+        Self {
+            aspect: width as f32 / height as f32,
+            fovy: fovy.into(),
+            znear,
+            zfar,
+        }
+    }
+
+    pub fn resize(&mut self, width: u32, height: u32) {
+        self.aspect = width as f32 / height as f32;
+    }
+
+    pub fn calc_matrix(&self) -> Matrix4<f32> {
+        OPENGL_TO_WGPU_MATRIX * perspective(self.fovy, self.aspect, self.znear, self.zfar)
     }
 }
 
 #[repr(C)]
-#[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+#[derive(Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
 struct CameraUniform {
-    view_projection: [[f32; 4]; 4],
+    view_position: [f32; 4],
+    view_proj: [[f32; 4]; 4],
 }
 
-impl From<&Camera> for CameraUniform {
-    fn from(cam: &Camera) -> Self {
+impl CameraUniform {
+    fn new() -> Self {
         Self {
-            view_projection: cam.build_view_projection_matrix().into(),
+            view_position: [0.0; 4],
+            view_proj: cgmath::Matrix4::identity().into(),
+        }
+    }
+
+    fn update_view_proj(&mut self, camera: &Camera, projection: &Projection) {
+        self.view_position = camera.position.to_homogeneous().into();
+        let cam_matrix = camera.calc_matrix();
+        self.view_proj = (projection.calc_matrix() * cam_matrix).into()
+    }
+}
+
+// TODO rewrite camera controller entirely
+#[derive(Debug)]
+pub struct CameraController {
+    amount_left: f32,
+    amount_right: f32,
+    amount_forward: f32,
+    amount_backward: f32,
+    amount_up: f32,
+    amount_down: f32,
+    rotate_horizontal: f32,
+    rotate_vertical: f32,
+    scroll: f32,
+    speed: f32,
+    sensitivity: f32,
+}
+
+// TODO why?
+const SAFE_FRAC_PI_2: f32 = FRAC_PI_2 - 0.0001;
+
+impl CameraController {
+    pub fn new(speed: f32, sensitivity: f32) -> Self {
+        Self {
+            amount_left: 0.0,
+            amount_right: 0.0,
+            amount_forward: 0.0,
+            amount_backward: 0.0,
+            amount_up: 0.0,
+            amount_down: 0.0,
+            rotate_horizontal: 0.0,
+            rotate_vertical: 0.0,
+            scroll: 0.0,
+            speed,
+            sensitivity,
+        }
+    }
+
+    pub fn process_keyboard(&mut self, key: KeyCode, state: ElementState) -> bool {
+        let amount = if state == ElementState::Pressed {
+            1.0
+        } else {
+            0.0
+        };
+        match key {
+            KeyCode::KeyW | KeyCode::ArrowUp => {
+                self.amount_forward = amount;
+                true
+            }
+            KeyCode::KeyS | KeyCode::ArrowDown => {
+                self.amount_backward = amount;
+                true
+            }
+            KeyCode::KeyA | KeyCode::ArrowLeft => {
+                self.amount_left = amount;
+                true
+            }
+            KeyCode::KeyD | KeyCode::ArrowRight => {
+                self.amount_right = amount;
+                true
+            }
+            KeyCode::Space => {
+                self.amount_up = amount;
+                true
+            }
+            KeyCode::ShiftLeft => {
+                self.amount_down = amount;
+                true
+            }
+            _ => false,
+        }
+    }
+
+    pub fn process_mouse(&mut self, mouse_dx: f64, mouse_dy: f64) {
+        self.rotate_horizontal = mouse_dx as f32;
+        self.rotate_vertical = mouse_dy as f32;
+    }
+
+    pub fn process_scroll(&mut self, delta: &MouseScrollDelta) {
+        self.scroll = -match delta {
+            // I'm assuming a line is about 100 pixels
+            MouseScrollDelta::LineDelta(_, scroll) => scroll * 100.0,
+            MouseScrollDelta::PixelDelta(PhysicalPosition { y: scroll, .. }) => *scroll as f32,
+        };
+    }
+
+    pub fn update_camera(&mut self, camera: &mut Camera, dt: Duration) {
+        let dt = dt.as_secs_f32();
+
+        // Move forward/backward and left/right
+        let (yaw_sin, yaw_cos) = camera.yaw.0.sin_cos();
+        let forward = Vector3::new(yaw_cos, 0.0, yaw_sin).normalize();
+        let right = Vector3::new(-yaw_sin, 0.0, yaw_cos).normalize();
+        camera.position += forward * (self.amount_forward - self.amount_backward) * self.speed * dt;
+        camera.position += right * (self.amount_right - self.amount_left) * self.speed * dt;
+
+        // Move up/down. Since we don't use roll, we can just
+        // modify the y coordinate directly.
+        camera.position.y += (self.amount_up - self.amount_down) * self.speed * dt;
+
+        // Rotate
+        camera.yaw += Rad(self.rotate_horizontal) * self.sensitivity * dt;
+        camera.pitch += Rad(-self.rotate_vertical) * self.sensitivity * dt;
+
+        // If process_mouse isn't called every frame, these values
+        // will not get set to zero, and the camera will rotate
+        // when moving in a non-cardinal direction.
+        self.rotate_horizontal = 0.0;
+        self.rotate_vertical = 0.0;
+
+        // Keep the camera's angle from going too high/low.
+        if camera.pitch < -Rad(SAFE_FRAC_PI_2) {
+            camera.pitch = -Rad(SAFE_FRAC_PI_2);
+        } else if camera.pitch > Rad(SAFE_FRAC_PI_2) {
+            camera.pitch = Rad(SAFE_FRAC_PI_2);
         }
     }
 }
@@ -68,30 +237,35 @@ struct State<'a> {
     diffuse_bind_group: wgpu::BindGroup,
 
     camera: Camera,
+    projection: Projection,
     camera_buffer: wgpu::Buffer,
     camera_bind_group: wgpu::BindGroup,
+    camera_controller: CameraController,
+    camera_uniform: CameraUniform,
+    num_vertices: usize, // TODO temp
+    mouse_pressed: bool,
 }
 
 const VERTICES: &[Vertex] = &[
     Vertex {
         position: [-0.0868241, 0.49240386, 0.0],
-        uv: [0.4131759, 0.99240386],
+        normal: [0.0, 0.4131759, 0.99240386],
     }, // A
     Vertex {
         position: [-0.49513406, 0.06958647, 0.0],
-        uv: [0.0048659444, 0.56958647],
+        normal: [0.0, 0.0048659444, 0.56958647],
     }, // B
     Vertex {
         position: [-0.21918549, -0.44939706, 0.0],
-        uv: [0.28081453, 0.05060294],
+        normal: [0.0, 0.28081453, 0.05060294],
     }, // C
     Vertex {
         position: [0.35966998, -0.3473291, 0.0],
-        uv: [0.85967, 0.1526709],
+        normal: [0.0, 0.85967, 0.1526709],
     }, // D
     Vertex {
         position: [0.44147372, 0.2347359, 0.0],
-        uv: [0.9414737, 0.7347359],
+        normal: [0.0, 0.9414737, 0.7347359],
     }, // E
 ];
 
@@ -267,27 +441,33 @@ impl<'a> State<'a> {
             source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
         });
 
+        let (vertices, indices) = create_mesh();
+
         let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Vertex Buffer"),
-            contents: bytemuck::cast_slice(VERTICES),
+            contents: bytemuck::cast_slice(&vertices),
             usage: wgpu::BufferUsages::VERTEX,
         });
 
         let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Index Buffer"),
-            contents: bytemuck::cast_slice(INDICES),
+            contents: bytemuck::cast_slice(&indices),
             usage: wgpu::BufferUsages::INDEX,
         });
 
-        let camera = Camera {
-            eye: (0.0, 1.0, 2.0).into(),
-            target: (0.0, 0.0, 0.0).into(),
-            up: cgmath::Vector3::unit_y(),
-            aspect: config.width as f32 / config.height as f32,
-            fovy: 0.79,
-            znear: 0.1,
-            zfar: 100.0,
-        };
+        let camera = Camera::new(Point3::new(0.0, 2.0, 1.0), Rad(0.0), Rad(0.0));
+        let projection = Projection::new(
+            /*width:*/ config.width,
+            /*height:*/ config.height,
+            /*fovy:*/ Rad(0.79),
+            /*znear:*/ 0.1,
+            /*zfar:*/ 100.0,
+        );
+
+        let camera_controller = CameraController::new(10.0, 4.0);
+
+        let mut camera_uniform = CameraUniform::new();
+        camera_uniform.update_view_proj(&camera, &projection);
 
         let camera_bind_group_layout =
             device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
@@ -306,7 +486,7 @@ impl<'a> State<'a> {
 
         let camera_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Camera Buffer"),
-            contents: bytemuck::cast_slice(&[Into::<CameraUniform>::into(&camera)]),
+            contents: bytemuck::cast_slice(&[camera_uniform]),
             usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
         });
 
@@ -341,7 +521,12 @@ impl<'a> State<'a> {
             diffuse_bind_group,
             camera,
             camera_buffer,
+            projection,
             camera_bind_group,
+            camera_controller,
+            camera_uniform,
+            mouse_pressed: false,
+            num_vertices: indices.len(),
         }
     }
 
@@ -351,14 +536,48 @@ impl<'a> State<'a> {
             self.config.width = new_size.width;
             self.config.height = new_size.height;
             self.surface.configure(&self.device, &self.config);
+            self.projection.resize(self.size.width, self.size.height);
+            // TODO update uniform
         }
     }
 
     fn input(&mut self, event: &WindowEvent) -> bool {
-        false
+        match event {
+            WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        physical_key: PhysicalKey::Code(key),
+                        state,
+                        ..
+                    },
+                ..
+            } => self.camera_controller.process_keyboard(*key, *state),
+            WindowEvent::MouseWheel { delta, .. } => {
+                self.camera_controller.process_scroll(delta);
+                true
+            }
+            WindowEvent::MouseInput {
+                button: MouseButton::Left,
+                state,
+                ..
+            } => {
+                self.mouse_pressed = *state == ElementState::Pressed;
+                true
+            }
+            _ => false,
+        }
     }
 
-    fn update(&mut self) {}
+    fn update(&mut self, dt: std::time::Duration) {
+        self.camera_controller.update_camera(&mut self.camera, dt);
+        self.camera_uniform
+            .update_view_proj(&self.camera, &self.projection);
+        self.queue.write_buffer(
+            &self.camera_buffer,
+            0,
+            bytemuck::cast_slice(&[self.camera_uniform]),
+        );
+    }
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
         let output = self.surface.get_current_texture()?;
@@ -395,8 +614,8 @@ impl<'a> State<'a> {
             render_pass.set_bind_group(0, &self.diffuse_bind_group, &[]);
             render_pass.set_bind_group(1, &self.camera_bind_group, &[]);
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
-            render_pass.draw_indexed(0..INDICES.len() as u32, 0, 0..1);
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint32);
+            render_pass.draw_indexed(0..self.num_vertices as u32, 0, 0..1);
         }
 
         // submit will accept anything that implements IntoIter
@@ -406,15 +625,108 @@ impl<'a> State<'a> {
         Ok(())
     }
 }
+fn noise_fract(x: f64, y: f64, octaves: usize, gen: &Perlin) -> f64 {
+    let mut acc = 0.0;
+    let lacunarity = 2.0;
+    let gain = 0.5;
+    let mut amplitude = 0.5;
+    let mut frequency = 1.;
+    for i in 1..octaves {
+        // acc = acc
+        //     + gen.get([
+        //         x / (2.0f64.powf(i as f64)) + i as f64 * 1000.0,
+        //         y / (2.0f64.powf(i as f64)) + i as f64 * 1000.0,
+        //     ]) * i as f64;
+
+        acc += amplitude
+            * gen.get([
+                frequency * x + i as f64 * 1000.0,
+                frequency * y + i as f64 * 1000.0,
+            ]);
+        frequency *= lacunarity;
+        amplitude *= gain;
+    }
+    return acc;
+}
+
+fn vec_addeq(a: &mut [f32], b: &[f32]) {
+    a[0] += b[0];
+    a[1] += b[1];
+    a[2] += b[2];
+}
+
+fn create_mesh() -> (Vec<Vertex>, Vec<u32>) {
+    let noise = Perlin::new(1);
+    let rad = 300;
+    let scl = 5;
+    let mut vertices = Vec::new();
+    let mut indices: Vec<u32> = Vec::new();
+    for y in 0..rad {
+        for x in 0..rad {
+            vertices.push(Vertex {
+                position: [
+                    x as f32 / scl as f32 - 0.5,
+                    noise_fract(x as f64 / 68.0, y as f64 / 68.0, 10, &noise) as f32 / 0.2,
+                    y as f32 / scl as f32 - 0.5,
+                ],
+                normal: [1.0, 0.0, 0.0],
+            });
+
+            // skip last point in each row or col as it has already been included in a triangle
+            if x == rad - 1 || y == rad - 1 {
+                continue;
+            }
+            // quad: 6 vertices
+            // first tri
+            indices.push((y + 1) * rad + x + 1);
+            indices.push(y * rad + x + 1);
+            indices.push(y * rad + x);
+            // second tri
+            indices.push((y + 1) * rad + x);
+            indices.push((y + 1) * rad + x + 1);
+            indices.push(y * rad + x);
+        }
+    }
+
+    for tri in indices.chunks(3) {
+        let (a, b, c) = (tri[0], tri[1], tri[2]);
+        let va = &vertices[a as usize].position;
+        let vb = &vertices[b as usize].position;
+        let vc = &vertices[c as usize].position;
+        let (a1, a2, a3) = (vb[0] - va[0], vb[1] - va[1], vb[2] - va[1]);
+        let (b1, b2, b3) = (vc[0] - va[0], vc[1] - va[1], vc[2] - va[1]);
+        let face_normal = [a2 * b3 - a3 * b2, a3 * b1 - a1 * b3, a1 * b2 - a2 * b1];
+        vec_addeq(&mut vertices[a as usize].normal, &face_normal);
+        vec_addeq(&mut vertices[b as usize].normal, &face_normal);
+        vec_addeq(&mut vertices[c as usize].normal, &face_normal);
+    }
+
+    for vertex in &mut vertices {
+        let inv_len = 1.0
+            / (vertex.normal[0] * vertex.normal[0]
+                + vertex.normal[1] * vertex.normal[1]
+                + vertex.normal[2] * vertex.normal[2])
+                .sqrt();
+        vertex.normal[0] *= inv_len;
+        vertex.normal[1] *= inv_len;
+        vertex.normal[2] *= inv_len;
+    }
+
+    (vertices, indices)
+}
 
 async fn run() {
     let event_loop = EventLoop::new().expect("Unable to create event loop!");
     let window = WindowBuilder::new().build(&event_loop).unwrap();
 
     let mut state = State::new(&window).await;
-
+    let mut last_render_time = std::time::Instant::now();
     event_loop
         .run(move |event, elwt| match event {
+            Event::DeviceEvent {
+                event: DeviceEvent::MouseMotion { delta },
+                ..
+            } => state.camera_controller.process_mouse(delta.0, delta.1),
             Event::WindowEvent {
                 ref event,
                 window_id,
@@ -426,7 +738,11 @@ async fn run() {
                             state.resize(*physical_size);
                         }
                         WindowEvent::RedrawRequested => {
-                            state.update();
+                            state.window.request_redraw();
+                            let now = std::time::Instant::now();
+                            let dt = now - last_render_time;
+                            last_render_time = now;
+                            state.update(dt);
                             match state.render() {
                                 Ok(_) => {}
                                 // Reconfigure the surface if lost
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,14 +1,15 @@
 struct VertexOutput {
     @builtin(position) clip_position: vec4<f32>,
-    @location(0) uv: vec2<f32>,
+    @location(0) normal: vec3<f32>,
 };
 
 struct VertexInput {
     @location(0) position: vec3<f32>,
-    @location(1) uv: vec2<f32>,
+    @location(1) normal: vec3<f32>,
 };
 
 struct CameraUniform {
+    view_pos: vec4<f32>,
     view_proj: mat4x4<f32>,
 };
 
@@ -18,7 +19,7 @@ fn vs_main(
 ) -> VertexOutput {
     var out: VertexOutput;
     out.clip_position = camera.view_proj * vec4<f32>(model.position, 1.0);
-    out.uv = model.uv;
+    out.normal = model.normal;
     return out;
 }
 
@@ -32,5 +33,6 @@ var<uniform> camera: CameraUniform;
 
 @fragment
 fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
-    return textureSample(t_diffuse, s_diffuse, in.uv);
+    // return textureSample(t_diffuse, s_diffuse, in.uv);
+    return vec4(in.normal, 1.0);
 }
